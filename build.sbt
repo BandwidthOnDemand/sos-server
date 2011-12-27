175ce50e9c7e1e36f338e0e6972770e8c429256f
Alan van Dam <avandam@zilverline.com>
2011-12-27 17:51:59 +0100

    simplified lift-json use, and check for user id

--- build.sbt
+++ build.sbt
@@ -12,8 +12,7 @@ port in container.Configuration := 8081
 
 libraryDependencies ++= Seq(
   "org.scalatra" %% "scalatra" % "2.0.2",
-  "org.scalatra" %% "scalatra-scalate" % "2.0.2",
-  "org.scalatra" %% "scalatra-specs2" % "2.0.2" % "test",
+  "org.scalatra" %% "scalatra-lift-json" % "2.0.2",
   "net.liftweb" %% "lift-json" % "2.4-M5",
   "ch.qos.logback" % "logback-classic" % "1.0.0" % "runtime",
   "org.eclipse.jetty" % "jetty-webapp" % "8.1.0.RC2" % "container",
--- src/main/scala/nl/surfnet/sos/web/OpenSocialServerController.scala
+++ src/main/scala/nl/surfnet/sos/web/OpenSocialServerController.scala
@@ -3,36 +3,37 @@ package nl.surfnet.sos.web
 import org.scalatra.ScalatraServlet
 import scala.xml.Xhtml
 import net.liftweb.json._
+import org.scalatra.liftjson.JsonSupport
 
-class OpenSocialServerController extends ScalatraServlet {
+class OpenSocialServerController extends ScalatraServlet with JsonSupport {
+
+  val groups = Map(
+      "urn:collab:person:surfguest.nl:alanvdam" -> List(
+        Group("bandwidth-on-demand", "test group"),
+        Group("institution-users", "users"),
+        Group("institution-users2", "users"),
+        Group("noc-engineer", "noc engineers"))
+      )
 
   get("/") {
     contentType = "text/html"
-
     Xhtml.toXhtml(<ul><li><a href="/rest/groups/@me">/rest/groups/@me</a></li></ul>)
   }
 
-  get("/rest/groups/@me") ({
+  get("/rest/groups/@me") {
     import net.liftweb.json.JsonDSL._
 
-    contentType = "application/json"
+    val user = params.getOrElse('xoauth_requestor_id, "anonymous")
+    val userGroups = groups.getOrElse(user, Nil)
 
-    val groups = List(
-        Group("bandwidth-on-demand", "test group"),
-        Group("institution-users3", "users"),
-        Group("institution-users2", "users"),
-        Group("noc-engineer", "noc engineers"))
-
-    compact(render(
-      ("startIndex" -> 0) ~
-      ("totalResults" -> groups.size) ~
-      ("entry" -> groups.map {group =>
-        (("id" -> ("groupId" -> group.groupId) ~ ("type" -> "groupId")) ~
-         ("title" -> group.title) ~
-         ("description" -> group.description)
-        )})
-    ))
-  })
+    ("startIndex" -> 0) ~
+    ("totalResults" -> userGroups.size) ~
+    ("entry" -> userGroups.map {group =>
+      (("id" -> ("groupId" -> group.groupId) ~ ("type" -> "groupId")) ~
+       ("title" -> group.title) ~
+       ("description" -> group.description))
+    })
+  }
 }
 
 case class Group(title: String, description: String) {
