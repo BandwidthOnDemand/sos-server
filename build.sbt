f9f05478e71998f905f407c8395a97c5bd51a6e0
Alan van Dam <alan@vandamnet.nl>
2011-12-27 13:56:08 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,23 @@
+organization := "nl.surfnet"
+
+name := "opensocial-server"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.9.1"
+
+seq(webSettings :_*)
+
+port in container.Configuration := 8081
+
+libraryDependencies ++= Seq(
+  "org.scalatra" %% "scalatra" % "2.0.2",
+  "org.scalatra" %% "scalatra-scalate" % "2.0.2",
+  "org.scalatra" %% "scalatra-specs2" % "2.0.2" % "test",
+  "net.liftweb" %% "lift-json" % "2.4-M5",
+  "ch.qos.logback" % "logback-classic" % "1.0.0" % "runtime",
+  "org.eclipse.jetty" % "jetty-webapp" % "8.1.0.RC2" % "container",
+  "javax.servlet" % "servlet-api" % "2.5" % "provided"
+)
+
+resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
---
+++ src/main/scala/nl/surfnet/osserver/web/OpenSocialServerController.scala
@@ -0,0 +1,36 @@
+package nl.surfnet.osserver.web
+
+import org.scalatra.ScalatraServlet
+import scala.xml.Xhtml
+import net.liftweb.json._
+
+class OpenSocialServerController extends ScalatraServlet {
+
+  get("/") {
+    Xhtml.toXhtml(<ul><li><a href="/rest/groups/@me"></a></li></ul>)
+  }
+
+  get("/rest/groups/@me") ({
+    import net.liftweb.json.JsonDSL._
+
+    contentType = "application/json"
+
+    val groups = List(
+        Group("bandwidth-on-demand", "test group"),
+        Group("noc-engineer", "noc engineers"))
+
+    compact(render(
+      ("startIndex" -> 0) ~
+      ("totalResults" -> groups.size) ~
+      ("entry" -> groups.map {group =>
+        (("id" -> ("groupId" -> group.groupId) ~ ("type" -> "groupId")) ~
+         ("title" -> group.title) ~
+         ("description" -> group.description)
+        )})
+    ))
+  })
+}
+
+case class Group(title: String, description: String) {
+  def groupId:String = "urn:collab:group:test.surfteams.nl:nl:surfnet:diensten:" + title
+}
