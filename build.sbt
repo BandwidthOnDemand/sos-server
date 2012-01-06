3a7597f235ebfbb31a811c787ead28dc3a147c1c
Alan van Dam <avandam@zilverline.com>
2012-01-06 16:32:55 +0100

    added support for a specific user and added tests

--- build.sbt
+++ build.sbt
@@ -10,13 +10,18 @@ seq(webSettings :_*)
 
 port in container.Configuration := 8081
 
-libraryDependencies ++= Seq(
-  "org.scalatra" %% "scalatra" % "2.0.2",
-  "org.scalatra" %% "scalatra-lift-json" % "2.0.2",
-  "net.liftweb" %% "lift-json" % "2.4-M5",
-  "ch.qos.logback" % "logback-classic" % "1.0.0" % "runtime",
-  "org.eclipse.jetty" % "jetty-webapp" % "8.1.0.RC2" % "container",
-  "javax.servlet" % "servlet-api" % "2.5" % "provided"
-)
+{
+  val scalatraVersion = "2.0.2"
+  libraryDependencies ++= Seq(
+    "org.scalatra" %% "scalatra" % scalatraVersion,
+    "org.scalatra" %% "scalatra-lift-json" % scalatraVersion ,
+    "org.scalatra" %% "scalatra-scalatest" % scalatraVersion % "test",
+    "net.liftweb" %% "lift-json" % "2.4-M5",
+    "ch.qos.logback" % "logback-classic" % "1.0.0" % "runtime",
+    "org.clapper" %% "grizzled-slf4j" % "0.6.6",
+    "org.eclipse.jetty" % "jetty-webapp" % "8.1.0.RC2" % "container",
+    "javax.servlet" % "servlet-api" % "2.5" % "provided"
+  )
+}
 
 resolvers += "Sonatype OSS Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
--- src/main/scala/nl/surfnet/sos/web/OpenSocialServerController.scala
+++ src/main/scala/nl/surfnet/sos/web/OpenSocialServerController.scala
@@ -1,30 +1,46 @@
-package nl.surfnet.sos.web
+package nl.surfnet.sos
+package web
 
 import org.scalatra.ScalatraServlet
 import scala.xml.Xhtml
 import net.liftweb.json._
 import org.scalatra.liftjson.JsonSupport
+import grizzled.slf4j.Logger
 
 class OpenSocialServerController extends ScalatraServlet with JsonSupport {
 
-  val groups = Map(
+  private val logger = Logger(classOf[OpenSocialServerController])
+
+  private val groups = Map(
       "urn:collab:person:surfguest.nl:alanvdam" -> List(
         Group("bandwidth-on-demand", "test group"),
+        Group("ict-managers", "ict-managers"),
         Group("institution-users", "users"),
         Group("institution-users2", "users"),
-        Group("noc-engineer", "noc engineers"))
+        Group("institution-users3", "Klimaat onderzoekers"),
+        Group("noc-engineer", "noc engineers")
+      ),
+      "urn:collab:person:surfguest.nl:okkeharsta" -> List(
+        Group("bandwidth-on-demand", "test group")
       )
+  )
 
   get("/") {
     contentType = "text/html"
     Xhtml.toXhtml(<ul><li><a href="/rest/groups/@me">/rest/groups/@me</a></li></ul>)
   }
 
-  get("/rest/groups/@me") {
+  get("/rest/groups/:guid") {
     import net.liftweb.json.JsonDSL._
 
-    val user = params.getOrElse('xoauth_requestor_id, "anonymous")
+    val user = params("guid") match {
+      case "@me" => params.getOrElse('xoauth_requestor_id, "anonymous")
+      case guid => guid
+    }
+
     val userGroups = groups.getOrElse(user, Nil)
+
+    logger.info("Groups request for '%s' returning %d groups".format(user, userGroups.size))
 
     ("startIndex" -> 0) ~
     ("totalResults" -> userGroups.size) ~
@@ -34,6 +50,7 @@ class OpenSocialServerController extends ScalatraServlet with JsonSupport {
        ("description" -> group.description))
     })
   }
+
 }
 
 case class Group(title: String, description: String) {
