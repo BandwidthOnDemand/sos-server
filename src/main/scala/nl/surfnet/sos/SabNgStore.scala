0313a9c503e9532b234a16324001f9a13cb5cef3
Alan van Dam <alan@vandamnet.nl>
2013-03-05 09:44:52 +0100

    only hanst is sabng infraverantwoordelijke for SURF

--- src/main/scala/nl/surfnet/sos/SabNgStore.scala
+++ src/main/scala/nl/surfnet/sos/SabNgStore.scala
@@ -5,14 +5,13 @@ object SabNgStore {
   private val userMap: Map[String, Map[String, Set[String]]] = Map(
     "urn:collab:person:surfguest.nl:johnsmith" ->
       Map(
-        "SURFNET" -> Set("Superuser", "Instellingsbevoegde", "Infraverantwoordelijke"),
-        "RUG" -> Set("Infraverantwoordelijke"),
+        "RUG" -> Set("Infraverantwoordelijke", "Instellingsbevoegde"),
         "SARA" -> Set("Superuser")
       ),
     "urn:collab:person:surfguest.nl:selenium-user" ->
       Map.empty,
     "urn:collab:person:surfnet.nl:hanst" ->
-      Map("SURFNET" -> Set("Infraverantwoordelijke"))
+      Map("SURFNET" -> Set("Infraverantwoordelijke", "Superuser"))
   )
 
   def getUser(nameId: String): Option[Map[String, Set[String]]] = userMap.get(nameId)
